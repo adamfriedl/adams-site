4b7ca3ea1cf735b0299e707382c55ddc9acc6aad
Adam Friedl <ajfriedl@gmail.com>
2017-07-15 22:00:21 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk'
+import App from './App';
+
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.devToolsExtension ? window.devToolsExtension() : f => f
+//   )
+// )
+
+ReactDOM.render(
+    <Provider>
+    <App /> 
+    </Provider>,
+    document.getElementById('root')
+)
+
